ce76365caa65ca506d927a9f07e9b85af942ea8f
ankur datta <dev.ankur.datta@gmail.com>
2023-01-17 17:29:23 +0600

    :bug: add different functions to compare start and end value

--- src/services/number-overlap-search.service.ts
+++ src/services/number-overlap-search.service.ts
@@ -2,12 +2,21 @@ import { Interval } from '../types';
 import { NumberStrategy } from './number-strategy.service';
 import { LineSweepOverlapStrategy } from './search-overlaps.service';
 
-export const numberEndTimesComparison = (a: Interval<number>, b: Interval<number>): number => {
-  return (a?.end ?? 0) - (b?.end ?? 0);
-};
+type Comparator = (a: Interval<number>, b: Interval<number>) => number;
+
+/**
+ *
+ * @param {keyof Interval<number>} p - either property of Interval
+ * @returns {Comparator} - a function which can be used as the comparator function for sorting or comparing ends
+ */
+export const numberIntervalComparison =
+  (p: keyof Interval<number>) =>
+  (a: Interval<number>, b: Interval<number>): number => {
+    return +(a?.[p] ?? 0) - +(b?.[p] ?? 0);
+  };
 
 export const OverlappingNumbersSearch = new LineSweepOverlapStrategy(
   NumberStrategy,
-  numberEndTimesComparison,
-  numberEndTimesComparison
+  numberIntervalComparison('end'),
+  numberIntervalComparison('start')
 );
