3b8da15ca229a7ccc427b191d1c44d6c75b503c5
ankur datta <dev.ankur.datta@gmail.com>
2023-01-17 15:28:06 +0600

    :bug: (services) reorder inclusive and exclusive logic

--- src/services/number-strategy.service.ts
+++ src/services/number-strategy.service.ts
@@ -6,7 +6,7 @@ export class NumberIntervalComparisonStrategy implements IntervalComparisonStrat
   isOverlapping(a: Interval<number>, b: Interval<number>, exclusive: boolean): boolean {
     const [from1, to1, from2, to2] = [a.start, a.end, b.start, b.end];
 
-    if (exclusive) {
+    if (!exclusive) {
       return (
         (isValidNumber(to2) ? from1 < (to2 as number) : true) &&
         (isValidNumber(to1) ? (to1 as number) > from2 : true)
